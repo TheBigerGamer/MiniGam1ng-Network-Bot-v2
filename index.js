8ce32129eae10e139d3d5386d03c13f24ac560b9
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-01-16 18:25:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -132,6 +132,20 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Não pude eliminar as mensagens por causa de: ${error}`));
   }
+  
+  if(command === "poll") {
+    if (!args) return message.reply("You must have something to vote for!")
+    if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
+    message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
+    const pollTopic = await message.channel.send(message.content.slice(2));
+    await pollTopic.react(`✅`);
+    await pollTopic.react(`⛔`);
+    // Create a reaction collector
+    const filter = (reaction) => reaction.emoji.name === '✅';
+    const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
+    collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+  }
 });
 
 client.login(config.token);
